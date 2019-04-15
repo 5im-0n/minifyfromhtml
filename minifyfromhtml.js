d1fd48f7e11f75045b6d6f03c726f8deadccdddb
s2 <s2@31337.it>
2019-04-15 10:39:58 +0200

    remove streamToString

--- minifyfromhtml.js
+++ minifyfromhtml.js
@@ -25,16 +25,6 @@ if (!argv.js || !argv.css) {
 	return;
 }
 
-function streamToString(stream) {
-	const chunks = []
-	return new Promise((resolve, reject) => {
-		stream.on('data', chunk => chunks.push(chunk))
-		stream.on('error', reject)
-		stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')))
-	})
-}
-
-
 var excludeFiles = argv.exclude || [];
 if (typeof(excludeFiles) === 'string') {
 	excludeFiles = [excludeFiles];
